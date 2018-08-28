63a39e6ff5582a0a7d80a23347595b92fed02589
eleroy <leroy.eric@gmail.com>
2018-08-28 08:40:02 -0700

    Initial commit

---
+++ app/src/main/java/com/wewow/pheramorregistration/models/WordRepository.java
@@ -0,0 +1,42 @@
+package com.wewow.pheramorregistration.models;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    public LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+
+    public void insert (Word word) {
+        new InsertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class InsertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        InsertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
